a04ece89d687c1643f9c924dbad4295b89191148
RavosonAndry <rivonirina.ra@gmail.com>
2026-06-12 09:25:25 +0300

    gitignore fix

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -8,14 +8,27 @@ class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        // On force Laravel à utiliser /tmp pour le cache et les vues sur Vercel
-        if (config('app.env') === 'production') {
+        // FORCE Laravel à utiliser /tmp sur Vercel
+        if (env('APP_ENV') === 'production') {
             $this->app->useStoragePath('/tmp');
         }
     }
 
     public function boot(): void
     {
-        //
+        // On s'assure que les dossiers nécessaires existent dans /tmp
+        if (env('APP_ENV') === 'production') {
+            $storagePaths = [
+                '/tmp/framework/views',
+                '/tmp/framework/cache',
+                '/tmp/framework/sessions',
+                '/tmp/app/public',
+            ];
+            foreach ($storagePaths as $path) {
+                if (!file_exists($path)) {
+                    mkdir($path, 0777, true);
+                }
+            }
+        }
     }
 }
