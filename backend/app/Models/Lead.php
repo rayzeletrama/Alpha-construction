7ad25aefe385f74c010ddf6d845eac4ef536e145
RavosonAndry <rivonirina.ra@gmail.com>
2026-06-14 21:26:28 +0300

    Lead ok

--- backend/app/Models/Lead.php
+++ backend/app/Models/Lead.php
@@ -1,8 +1,13 @@
+<?php
+
 namespace App\Models;
+
 use App\Traits\BelongsToTenant;
 use Illuminate\Database\Eloquent\Model;
 
-class Lead extends Model {
+class Lead extends Model
+{
     use BelongsToTenant;
+
     protected $fillable = ['name', 'email', 'subject', 'message', 'status'];
 }
